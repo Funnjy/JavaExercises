3b6e2b114debf377b2686051401851c1f28253ef
Anton Ilchenko <ilchenko.anton@gmail.com>
2017-03-19 23:06:14 +0100

    fjernSiste()
    Function added

--- W_Exercises 1/src/Node.java
+++ W_Exercises 1/src/Node.java
@@ -12,7 +12,7 @@ class Node
 {
   private int info;
   Node neste;
-
+  private Node første = neste;
   public Node( int data, Node etterfølger )
   {
     info = data;
@@ -27,5 +27,22 @@ class Node
   public void setInfo( int nyVerdi )
   {
     info = nyVerdi;
+  }
+  public void fjernSiste(){
+     /*Se om første lement er ikke 0(ikke tom liste)*/
+     if(første != null){
+          /*Se om det andre element er null(1 element i liste)*/
+          if(første.neste == null){
+              første = null;
+          }
+          else
+              while(første.neste.neste != null){
+                  første = første.neste;
+              }
+              første.neste = null;
+     }
+     /*Begynn å gå gjennom alle elementer*/
+     /*sjekk om neste element er det siste*/
+     /*hvis ja,  fjern. Ellers fortsett*/
   }
 }
