c139c9119385a66c9684381b1477e0aa6e6d779b
Anton <ilchenko.anton@gmail.com>
2017-03-29 18:46:28 +0200

    Basics dialogboks

--- Basics/src/main.java
+++ Basics/src/main.java
@@ -2,7 +2,7 @@
  * Created by Anton on 29.03.2017.
  */
 public class main {
-    public static void main(String[] args){
+   /* public static void main(String[] args){
         System.out.println("");
-    }
+    }*/
 }
