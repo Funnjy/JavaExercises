47dfbf9acd0089062923a8d1736ae324ccfbb810
Anton Ilchenko <ilchenko.anton@gmail.com>
2017-03-30 15:33:34 +0200

    Try

--- Basics/src/Dialogboks.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Anton on 29.03.2017.
- */
-import javax.swing.JOptionPane;
-public class Dialogboks {
-    public static class main(String[] args){
-        JOptionPane.showMessageDialog(null,"hello\n");
-    }
-}
--- Basics/src/main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Anton on 29.03.2017.
- */
-public class main {
-   /* public static void main(String[] args){
-        System.out.println("");
-    }*/
-}
